d71f83a3ac3ee954aee2207fca979af353653445
11nstz <1361125611@qq.com>
2020-03-12 20:15:46 +0800

    单词学习

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+
+namespace danci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello 郑俊豪，按t退出，回车继续");
+            Program ruko = new Program();
+            ruko.duqu();
+            
+        }
+        private void duqu()
+        {    string url3= Console.ReadLine();
+             string url1 =@"D:\vscode\c#\danci\dcb\";
+            string url2 = ".txt";
+            url1=url1+url3+url2;
+            StreamReader reader = new StreamReader(url1,Encoding.Unicode);
+            string line;
+            int flag1=0;
+            while((line=reader.ReadLine())!=null && flag1==0)
+            {Console.WriteLine(line);
+            string tuichu =Console.ReadLine();
+             if(tuichu=="t"){flag1=1;}}
+             }
+      
+    }
+}
