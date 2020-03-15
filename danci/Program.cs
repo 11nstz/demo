f568c27243ac233916de80a4d8fb5203cc281e0b
11nstz <1361125611@qq.com>
2020-03-15 14:34:51 +0800

    增加异常判断和计数

--- danci/Program.cs
+++ danci/Program.cs
@@ -3,25 +3,33 @@ using System.Text;
 using System;
  namespace duqu 
  {
-    class duqu {
-        static void Main() {  
+    class duqu 
+    {
+        static void Main() 
+        { 
+        star : 
+       try{
         Console.WriteLine("hello 郑俊豪,请输入想背的单词表，回车继续，t退出");
         string url1=@".\dcb\"+Console.ReadLine()+".txt";
         StreamReader reader = new StreamReader(url1,Encoding.Unicode);
         string line ;
         bool point = true;
         Console.WriteLine("从第几个开始？");
-        int star =(int)Console.Read()*2-1;
+        int star =int.Parse(Console.ReadLine())*2-1;
         int number =0;
-        while((line = reader.ReadLine()) != null && point  )
-        { number+=1;
-        if(number >=star)
-               {Console.WriteLine(line);
-                string s=Console.ReadLine();
-                if (s =="t" ){point=false;}
-               }
-          }
+            while((line = reader.ReadLine()) != null && point  )
+              { number+=1;
+                  if(number >=star)
+                    {Console.WriteLine(line+" "+(number+1)/2);
+                    string s=Console.ReadLine();
+                      if (s =="t" ){point=false;}
+                       else if (s=="z"){goto star;}
+                    }
+                }
         }
+        catch{Console.WriteLine("存在错误，请重新输入"); goto star;}
+        }
+        
         
     }
 
