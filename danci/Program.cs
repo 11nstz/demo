70cc70bc7d0016638e805ce13a6e3efc01ad2e28
11nstz <1361125611@qq.com>
2020-03-16 09:42:12 +0800

    优化结构

--- danci/Program.cs
+++ danci/Program.cs
@@ -7,18 +7,29 @@ namespace duqu
     {
         static void Main()
         {
-        star:
-            try
+            meth m1 = new meth();
+            while()
             {
-                Console.WriteLine("hello 郑俊豪,请输入想背的单词表，回车继续，t退出\nz转换到其他字母表");
+            Console.WriteLine("hello 郑俊豪,请输入想背的单词表，回车继续，t退出\nz转换到其他字母表");
+            StreamReader reader = new StreamReader(m1.geturl(), Encoding.Unicode);
+            m1.outputword(reader);
+            }
+        }
+        class meth
+        {
+            public string geturl()
+            {
                 string url1 = @".\dcb\" + Console.ReadLine() + ".txt";//获取字母表
-                StreamReader reader = new StreamReader(url1, Encoding.Unicode);
+                return url1;
+            }
+            public void outputword(StreamReader a)
+            {
                 string line;//缓存字符
                 bool point = true;//表示是否退出
                 Console.WriteLine("从第几个开始？");
                 int star = int.Parse(Console.ReadLine()) * 2 - 1;//换算第几个单词开始
                 int number = 0;//记录当前读取位置
-                while ((line = reader.ReadLine()) != null && point)
+                while ((line = a.ReadLine()) != null && point)
                 {
                     number += 1;
                     if (number >= star)
@@ -26,12 +37,16 @@ namespace duqu
                         Console.WriteLine(line + " " + (number + 1) / 2);//标记当前位置并输出
                         string s = Console.ReadLine();
                         if (s == "t") { point = false; }//接受退出信息
-                        else if (s == "z") { goto star; }//接受换表信息
+                        else if (s == "z") {  }//接受换表信息
                     }
                 }
             }
-            catch { Console.WriteLine("存在错误，请重新输入"); goto star; }
+            private string checkinput(){}
         }
+
+
+
+
 
 
     }
